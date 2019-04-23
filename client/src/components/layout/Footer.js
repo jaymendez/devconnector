b18bab1832bb593cc83cfc95fe4d10dc3cc49c24
Jay Mendez <johnjosephmendez2016@gmail.com>
2019-04-23 11:01:07 +0800

    Added react, added router for login, register, home, profiles

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <footer className="bg-dark text-white mt-5 p-4 text-center">
+            Copyright &copy; {new Date().getFullYear()} Dev Connector
+        </footer>
+    </div>
+  )
+}
+
+export default Footer;
